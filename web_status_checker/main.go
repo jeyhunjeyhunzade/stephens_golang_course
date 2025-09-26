1a1c9c6610a61fc0c69c0b2cc4454b803ee431ec
j.jeyhunzade <j.jeyhunzade@xsolla.com>
2025-09-26 17:56:48 +0400

    learning: play with go routines & channels

---
+++ web_status_checker/main.go
@@ -0,0 +1,54 @@
+//channels exercise
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main () {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// for { // infinite loop
+	// 	go checkLink(<-c, c)
+	// }
+
+	// more readable version
+	for l := range c {
+		go checkLink(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err	:= http.Get(link) 
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		// c <- "Might be down"
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	// c <- "It's up"
+	c <- link
+}
